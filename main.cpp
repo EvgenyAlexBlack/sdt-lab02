f136e9c4ecfa411deef9131b067eb7e84528e9b1
EvgenyAlex <0543398@gmail.com>
2023-10-05 09:55:58 +0300

    Добавлена печать произведения чисел

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-a = int(input("Enter A: "))
-b = int(input("Enter B: "))
+def main():
+    a = int(input("Enter A: "))
+    b = int(input("Enter B: "))
+    
+    print(f"A + B = {a + b}")
+    print(f"A * 2 = {a * 2}")
+    print(f"A - B = {a - b}")
+    print(f"A * B = {a * b}")
+    print(f"A / B = {a / b}")  # Деление в Python
 
-print(f"A + B = {a + b}")
-print(f"A * 2 = {a * 2}")
-print(f"A - B = {a - b}")
-print(f"A * B = {a * b}")
-print(f"A / B = {a / b}")
+if __name__ == "__main__":
+    main()
