adca20831781f753918ce0a9ee7d6966880d0a09
EvgenyAlex <0543398@gmail.com>
2023-10-05 09:53:44 +0300

    Добавлена печать произведения чисел

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
-number = 1
-result = number * 2
-print(result)
+a = int(input("Enter A: "))
+b = int(input("Enter B: "))
+
+print(f"A + B = {a + b}")
+print(f"A * 2 = {a * 2}")
+print(f"A - B = {a - b}")
+print(f"A * B = {a * b}")
+print(f"A / B = {a / b}")
